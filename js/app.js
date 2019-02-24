439ccfe2554b4974a82163e493496aedf45e4fd4
Jess Manapul <Jess@Jess.local>
2019-02-23 16:44:09 -1000

    incorporated 2 functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+function changeColor() {
+    // console.log('hi');
+    var choices = '0123456789ABCDEF';
+    var color = '#';
 
+    for (var i = 0; i < 6; i++) {
+        var randomNumber = Math.floor(Math.random() * 16); 
+        color += choices[randomNumber];
+       // console.log(color);
+    }
+   // console.log(color);
+    document.body.style.backgroundColor = color;
+    title.style.color = 'white';
+}
+
+
+function reset() {
+    document.body.style.backgroundColor = '#eee';
+    title.style.color = 'black';
+}
